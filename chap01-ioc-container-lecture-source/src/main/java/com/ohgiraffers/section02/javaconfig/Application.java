7b0785b26155b672f90520d620ef48c1351b61db
euuuuuuan <euan.may24@gmail.com>
2024-07-04 15:49:07 +0900

    IOC

--- chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -13,7 +13,8 @@ public class Application {
 
         // ID 값을 기존 메소드명 getMember가 아닌 @Bean 안에 괄호를 통해 설정해줄 수 있다. 'member'로 호출함.
         MemberDTO member2 = context.getBean("member", MemberDTO.class);
-        
+
         System.out.println(member);
+        System.out.println(member2);
     }
 }
