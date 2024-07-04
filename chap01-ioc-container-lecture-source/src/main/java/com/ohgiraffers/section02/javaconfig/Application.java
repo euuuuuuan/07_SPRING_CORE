7345975e689ea95739d56dcfaa4de55f4e28bf23
euuuuuuan <euan.may24@gmail.com>
2024-07-04 16:59:33 +0900

    IOC - Container

--- chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -9,12 +9,12 @@ public class Application {
         ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
 
 
-        MemberDTO member = context.getBean("getMember", MemberDTO.class);
+//        MemberDTO member = context.getBean("getMember", MemberDTO.class);
 
         // ID 값을 기존 메소드명 getMember가 아닌 @Bean 안에 괄호를 통해 설정해줄 수 있다. 'member'로 호출함.
         MemberDTO member2 = context.getBean("member", MemberDTO.class);
 
-        System.out.println(member);
+//        System.out.println(member);
         System.out.println(member2);
     }
 }
