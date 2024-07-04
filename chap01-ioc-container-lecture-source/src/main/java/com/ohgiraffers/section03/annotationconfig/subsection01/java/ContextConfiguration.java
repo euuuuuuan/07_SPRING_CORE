bd24037cdd668dc72cc309178bd0677eb0eb7db4
euuuuuuan <euan.may24@gmail.com>
2024-07-04 16:09:42 +0900

    IOC

--- chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection01/java/ContextConfiguration.java
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection01/java/ContextConfiguration.java
@@ -1,4 +1,10 @@
 package com.ohgiraffers.section03.annotationconfig.subsection01.java;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "com.ohgiraffers") // 해당 경로의 하위 경로를 전부 스캔
+@Configuration("contextConfiguration3")
 public class ContextConfiguration {
+
 }
